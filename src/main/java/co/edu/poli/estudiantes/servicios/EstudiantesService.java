4dc506ede4dc782498b1e759a8b461b4b4e376ed
Davidm-rodriguez <davidm.rodriguez@me.com>
2021-03-18 20:56:08 -0500

    Primer Proyecto Estudiantes

---
+++ src/main/java/co/edu/poli/estudiantes/servicios/EstudiantesService.java
@@ -0,0 +1,10 @@
+package co.edu.poli.estudiantes.servicios;
+
+import java.util.List;
+
+import co.edu.poli.estudiantes.dto.EstudiantesDTO;
+
+public interface EstudiantesService {
+	public List<EstudiantesDTO> findAll();
+
+}
